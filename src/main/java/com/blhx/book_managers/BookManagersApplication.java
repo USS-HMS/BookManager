d88892bcb0da2b796722739e9f2910770c962f73
BLHX <2819208060@qq.com>
2020-03-28 03:17:20 +0800

    first commit

---
+++ src/main/java/com/blhx/book_managers/BookManagersApplication.java
@@ -0,0 +1,13 @@
+package com.blhx.book_managers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagersApplication {
+
+    public static void main ( String[] args ){
+        SpringApplication.run(BookManagersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blhx/book_managers/BookManagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blhx.book_managers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagersApplicationTests {
+
+    @Test
+    void contextLoads ( ){
+    }
+
+}
